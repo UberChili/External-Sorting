4e5a322a1356cfa8776875b93315c3bc50f0624e
Andres Nunez <nunezlopez.ja@gmail.com>
2017-12-14 01:02:27 -0600

    Agregada función chop_sort(), Aún incompleta

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from random import randint
+import math
 
 def main():
     size = int(input("Size: "))
 
     generate_file(size)
+    chop_sort(size)
 
 def generate_file(size):
 
@@ -12,6 +14,14 @@ def generate_file(size):
     for line in range(size):
         fp.write(str(randint(0, 100)))
         fp.write("\n")
+
+    fp.close()
+
+def chop_sort(size):
+    np = math.ceil(size/10)
+    print(np)
+
+    fp = open("file.txt", "r")
 
     fp.close()
 
